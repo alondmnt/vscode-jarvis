fd3abee8eaa8cac0286ff466b06e2775aea6b07e
alondmnt <alondmnt@users.noreply.github.com>
2023-03-11 10:51:30 +0200

    fix: text insertion fix

--- src/utils.ts
+++ src/utils.ts
@@ -2,12 +2,19 @@
 import * as vscode from 'vscode';
 
 export function replace_selection(text: string) {
-    const editor = vscode.window.activeTextEditor;
-    if (editor) {
-      const selection = editor.selection;
-      // replace selection in the editor with the citation
+  const editor = vscode.window.activeTextEditor;
+  if (editor) {
+    const selection = editor.selection;
+    // if selection is empty then insert text at cursor
+    if (selection.isEmpty) {
       editor.edit((editBuilder) => {
-        editBuilder.replace(selection, text);
+        editBuilder.insert(selection.active, text);
       });
+      return;
+    }
+    // replace selection in the editor with the citation
+    editor.edit((editBuilder) => {
+      editBuilder.replace(selection, text);
+    });
     }
 }
